625a67c679bc46caf553c05d16ab522e32d03330
Matthew Holmes <mlholmes91@gmail.com>
2020-04-11 16:02:45 -0600

    initialize c sharp learning projects

---
+++ FirstConsoleApp/FirstConsoleApp/Program.vb
@@ -0,0 +1,10 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("Press enter to continue")
+        Console.ReadLine()
+
+    End Sub
+End Module
